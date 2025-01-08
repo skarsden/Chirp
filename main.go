2f1addc65f13acd1b5bca3a281aec388a08b70a8
skarsden <zachfrancis98@gmail.com>
2025-01-08 16:04:32 -0500

    database configured with postgres, goose, and sqlc

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/skarsden/Chirp/internal/database"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	queries        *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbUrl := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Printf("Error opening sql database: %s", err)
+	}
+	dbQueries := database.New(db)
+
 	const port = "8080"
 	const root = "."
 
 	//records number of handler calls
 	apiCfg := apiConfig{
 		fileServerHits: atomic.Int32{},
+		queries:        dbQueries,
 	}
 
 	//Declare handler and register handler functions
